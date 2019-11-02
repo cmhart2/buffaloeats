6eb84a722067b1a958d729add5ac69a2a408af48
slake3 <50087241+slake3@users.noreply.github.com>
2019-11-02 16:33:47 -0400

    Add files via upload

---
+++ foodMessage.scala
@@ -0,0 +1,27 @@
+import play.api.libs.json.{JsValue, Json}
+
+object foodMessage {
+
+  def fromJSON(json: String): FoodMessage = {
+    val jsonValue = Json.parse(json)
+    val restaurantName = (jsonValue \ "restaurant").as[String]
+    val restaurantAddress= (jsonValue \ "address").as[String]
+    val latitude= (jsonValue \ "latitude").as[String]
+    val longitude=(jsonValue \ "longitude").as[String]
+    val load=(jsonValue \ "load size").as[String]
+    val bank=(jsonValue \ "nearest food bank").as[String]
+    new FoodMessage(restaurantName, restaurantAddress, latitude, longitude, load, bank)
+  }
+
+}
+
+class FoodMessage(val rName: String, val address: String, val longitude: String, val latitude: String,
+                  loadSize: String, val nearestBank: String) {
+
+  def asJsValue(): JsValue ={
+    val messageMap: Map[String, JsValue] = Map("restaurant" -> Json.toJson(rName), "address" -> Json.toJson(address),
+    "longitude" -> Json.toJson(longitude), "latitude" -> Json.toJson(latitude), "load size" -> Json.toJson(loadSize),
+    "nearest food bank" -> Json.toJson(nearestBank))
+    Json.toJson(messageMap)
+  }
+}
